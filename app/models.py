718f06820ccca7126f884d9770dcf613577cb355
mosson <136782643@qq.com>
2019-06-18 09:07:15 +0800

    follows

--- app/models.py
+++ app/models.py
@@ -12,6 +12,13 @@ from app import db, login
 def load_user(id):
     return User.query.get(int(id))
 
+
+followers = db.Table('followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -20,20 +27,51 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-    def set_password(self, password):
+    def set_password(self, password): # 密码加密
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
-    def avatar(self, size):
+    def avatar(self, size): # 头像
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
             digest, size)
+
+    def follow(self, user):  # 关注
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):  # 取消关注
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):  # 是否关注
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
+    # def followed_posts(self):
+        # return Post.query.join(
+        #     followers, (followers.c.followed_id == Post.user_id)).filter(
+        #     followers.c.follower_id == self.id).order_by(
+        #     Post.timestamp.desc())
+
+    def followed_posts(self):
+        followed = Post.query.join(
+            followers, (followers.c.followed_id == Post.user_id)).filter(
+            followers.c.follower_id == self.id)
+        own = Post.query.filter_by(user_id=self.id)
+        return followed.union(own).order_by(Post.timestamp.desc())
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
